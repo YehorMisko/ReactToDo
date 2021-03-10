d8ecd0a78d57c64d46f11399af2141f38dcfa777
Yehor Misko <jayden.owem@gmail.com>
2021-03-10 13:09:49 +0200

    Lesha Gay

---
+++ ReactASPCrud/ReactASPCrud/Controllers/ToDoItemsController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using ReactASPCrud.Models;
+using ReactASPCrud.Services;
+namespace ReactASPCrud.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("ReactPolicy")]
+    public class ToDoItemsController : ControllerBase
+    {
+        private readonly ToDoItemService userService;
+        public ToDoItemsController(ToDoItemService userService)
+        {
+            this.userService = userService;
+        }
+        // GET api/users
+        [HttpGet]
+        public IEnumerable<ToDoItem> Get()
+        {
+            return userService.GetAll();
+        }
+        // GET api/users/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok(userService.GetById(id));
+        }
+        // POST api/users
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] ToDoItem action)
+        {
+            return CreatedAtAction("Get", new { id = action.Id }, userService.Create(action));
+        }
+        // PUT api/users/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] ToDoItem action)
+        {
+            userService.Update(id, action);
+            return NoContent();
+        }
+        // DELETE api/users/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            userService.Delete(id);
+            return NoContent();
+        }
+        [HttpPatch("{id}")]
+        public async Task<IActionResult> Patch(int id, [FromBody] ToDoItem action)
+        {
+            userService.ChangeState(id, action);
+            return NoContent();
+        }
+        public override NoContentResult NoContent()
+        {
+            return base.NoContent();
+        }
+    }
+}
---
+++ ReactASPCrud/ReactASPCrud/Services/ToDoItemService.cs
@@ -0,0 +1,63 @@
+﻿using ReactASPCrud.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace ReactASPCrud.Services
+{
+    public class ToDoItemService
+    {
+        private static List<ToDoItem> ToDoList = new List<ToDoItem>();
+        private static int Count = 1;
+        private static readonly string[] names = new string[] { "Wake up", "Put on a little bit of make up", "Table", "Get to Valhalla", "Sacrifice the child to Odin", "Corrupt the child", "Spend money on micro-transactions", "The concept" };
+        static ToDoItemService()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                string currName = names[rnd.Next(names.Length)];
+                ToDoItem action = new ToDoItem
+                {
+                    Id = Count++,
+                    Name = currName,
+                    IsDone = false
+                };
+                ToDoList.Add(action);
+            }
+        }
+        public List<ToDoItem> GetAll()
+        {
+
+
+            var query = from ToDoItem in ToDoList
+                        orderby ToDoItem.IsDone ascending
+                        select ToDoItem;
+
+            return query.ToList();
+        }
+        public ToDoItem GetById(int id)
+        {
+            return ToDoList.Where(user => user.Id == id).FirstOrDefault();
+        }
+        public ToDoItem Create(ToDoItem action)
+        {
+            action.Id = Count++;
+            ToDoList.Add(action);
+            return action;
+        }
+        public void Update(int id, ToDoItem action)
+        {
+            ToDoItem found = ToDoList.Where(n => n.Id == id).FirstOrDefault();
+            found.Name = action.Name;
+            found.IsDone = action.IsDone;
+        }
+        public void ChangeState(int id, ToDoItem action)
+        {
+            ToDoItem found = ToDoList.Where(n => n.Id == id).FirstOrDefault();
+            found.IsDone = !action.IsDone;
+        }
+        public void Delete(int id)
+        {
+            ToDoList.RemoveAll(n => n.Id == id);
+        }
+    }
+}
